e0e7d89827e2595f748e972e815e6ec42c1fb178
Oberhofer Fabio <fabio.oberhofer@bbcag.ch>
2022-12-29 13:55:07 +0100

    src: fix flexlayout and changed color

--- SolarManager/Views/MauiProgram.cs
+++ SolarManager/Views/MauiProgram.cs
@@ -18,8 +18,7 @@ public static class MauiProgram
             .ConfigureSyncfusionCore()
             .ConfigureFonts(fonts =>
             {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Segoe-UI.ttf", "SegoeUI");
             });
 
 #if DEBUG
--- SolarManager/Views/graphsExtra.xaml.cs
+++ SolarManager/Views/graphsExtra.xaml.cs
@@ -1,4 +1,5 @@
 using SolarManager.Models;
+using Syncfusion.Maui.Charts;
 
 namespace SolarManager;
 
@@ -6,6 +7,36 @@ public partial class graphsExtra : ContentPage
 {
 	public graphsExtra()
 	{
+        this.BindingContext = new ViewModel();
         InitializeComponent();
+
+
+
+
+        SfCartesianChart chart = new SfCartesianChart();
+
+        DateTimeAxis primaryAxis = new DateTimeAxis();
+        chart.XAxes.Add(primaryAxis);
+
+        NumericalAxis secondaryAxis = new NumericalAxis();
+        chart.YAxes.Add(secondaryAxis);
+
+        ColumnSeries series = new ColumnSeries();
+        series.Label = "KW";
+        series.ShowDataLabels = true;
+        series.ItemsSource = (new ViewModel()).Data;
+        series.XBindingPath = "Time";
+        series.YBindingPath = "KW";
+
+        chart.Series.Add(series);
+
+        SfCircularChart circ = new SfCircularChart();
+        ViewModel viewModel = new ViewModel();
+        circ.BindingContext = viewModel;
+        PieSeries sr = new PieSeries();
+        sr.ItemsSource = viewModel.Data;
+        sr.XBindingPath = "Time";
+        sr.YBindingPath = "KW";
+        circ.Series.Add(sr);
     }
 }
